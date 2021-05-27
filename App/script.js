bee0f7a253b0e7787ffd4f62ab85efacda3a713d
Tiago-1001 <tfarias059@gmail.com>
2021-05-26 21:13:39 -0300

    Modificações no design do projeto

--- App/script.js
+++ App/script.js
@@ -17,28 +17,28 @@ for(var i = 0; i <= 60; i++){
     segundos.innerHTML+='<option value="'+i+'"> '+i+'</option>';
 }
 
-iniciar.addEventListener('click', function(){
+iniciar.addEventListener('click', function () {
     minutoAtual = minutos.value;
     segundoAtual = segundos.value;
 
-    display.childNodes[1].innerHTML = minutoAtual + ":"+segundoAtual;
+    display.childNodes[1].innerHTML = minutoAtual + ":" + segundoAtual;
 
-    interval = setInterval(function(){
+    interval = setInterval(function () {
 
         segundoAtual--;
-        if(segundoAtual <=0){
-            if(minutoAtual > 0){
+        if (segundoAtual <= 0) {
+            if (minutoAtual > 0) {
                 minutoAtual--;
                 segundoAtual = 59;
             }
-            else{
+            else {
                 alert("Fim da Contagem");
                 /*document.getElementById("sound").play();*//*Espaço para por o som*/
                 clearInterval(interval);
             }
         }
 
-        display.childNodes[1].innerHTML = minutoAtual + ":"+segundoAtual;
+        display.childNodes[1].innerHTML = minutoAtual + ":" + segundoAtual;
 
-    },1000);
-)
+    }, 1000);
+})
